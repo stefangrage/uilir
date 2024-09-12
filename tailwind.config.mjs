0c075ebd338c732c1026335b6d187c5c2ff430dd
stefangrage <hej@stefangrage.dk>
2024-09-12 09:12:12 +0200

    Tilføjer lidt lir;-)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            cursor: {
+                // 'sjaeldencursor': 'url(cursor.svg) 25 25, pointer'
+                'handcursor': 'url(cursor.svg) 25 25, pointer',
+            }
+        },
 	},
 	plugins: [
         require('@tailwindcss/typography'),
